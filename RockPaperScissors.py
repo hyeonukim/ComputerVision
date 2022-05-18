498dad3225b25ce4bd6525a0a53efebed7d668bc
hyeonukim <48634064+hyeonukim@users.noreply.github.com>
2022-05-18 00:07:43 -0700

    handtracking module is now applied

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,21 +1,30 @@
+from json import detect_encoding
 import cv2
 import mediapipe as mp
 import time
-import HandTrackingModule as htm
+import os
+import HandTrackingModule as hm
+
+wCam, hCam = 640, 480
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
 
 pTime = 0
-cTime = 0
-cap = cv2.VideoCapture(0)
-detector = htm.handDetector()
+detector = hm.handDetector(detectionCon=0.75)
+
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
-    lmList = detector.findPosition(img)
-    # if len(lmList) != 0:
-    #     print(lmList[4])
+    lmList = detector.findPosition(img, draw=False)
+
+    print(lmList)
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
-    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+
+    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,0), 2)
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
